a799564cd4eda98d648f626ab1625b46b334ed6d
ezambomsantana <ezambomsantana@gmail.com>
2017-06-13 09:53:48 -0300

    Creating the parser to read the bus XML.

---
+++ src/bus_parser.erl
@@ -0,0 +1,105 @@
+-module(bus_parser).
+-include_lib("xmerl/include/xmerl.hrl").
+
+-export([
+         show/1
+        ]).
+
+% Init the XML processing
+show(Infilename) ->
+    {Doc, _Misc} = xmerl_scan:file(Infilename),
+    init(Doc).
+
+% read the OSM tag and extract all children
+init(Node) ->
+    case Node of
+        #xmlElement{name=Name, content=Content} ->
+            
+	    case Name of
+		
+		scsimulator_buses -> 
+
+			List = buses(Content , [] ),
+			List;
+
+		_ -> ok
+
+	    end;
+            _ -> ok
+    end.
+
+buses([], List ) ->
+    List;
+
+buses([Node | MoreNodes] , List ) ->
+    Element = extract_node( Node ),
+    case Element of
+
+	ok ->
+    		
+		buses( MoreNodes , List );
+
+	_ ->
+		NewList = List ++ Element,
+		buses( MoreNodes , NewList )
+
+    end.
+
+%
+% Show a node/element and then the children of that node.
+extract_node( Node ) ->
+
+    case Node of
+        #xmlElement{ name=Name , content=_Content , attributes=Attributes } ->
+            
+	    case Name of
+		
+		bus -> 
+			
+				Id = children( Attributes , id ),
+				Interval = children( Attributes , interval ),
+				Stops = children( Attributes , stops ),
+				StartTime = children( Attributes , start_time ),
+				[ { Id , Interval , Stops , StartTime } ];
+		_ ->
+			
+			ok
+
+	    end;
+
+            _ -> ok
+    end.
+
+children( [] , _Type ) ->
+    ok;
+
+children( [Node | MoreNodes] , Type ) ->
+    Element = extract_children( Node , Type ),
+    case Element of
+
+	ok ->
+    		
+		children( MoreNodes , Type );
+
+	_ ->
+		Element
+
+    end.
+
+
+extract_children( Node , Type ) ->
+
+    case Node of
+        #xmlAttribute{name=Name, value=Value} ->
+            
+	    case Name of
+		
+		Type -> 
+
+			Value;
+
+		_ -> ok
+
+	    end;
+            _ -> ok
+    end.
