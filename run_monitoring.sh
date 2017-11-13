e198a3019602d9a6dc5266c35c18341b69b59a58
ezambomsantana <ezambomsantana@gmail.com>
2017-11-13 10:07:16 -0200

    monitoramento

---
+++ run_monitoring.sh
@@ -0,0 +1,22 @@
+
+timestamp() {
+  date +"%T"
+}
+
+timestamp
+
+
+max=100
+for i in `seq 2 $max`
+do
+
+
+	free -m | awk 'NR==2{printf "%s, %s, %.2f%%", $3,$2,$3*100/$2 }'
+
+	grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print "," usage "%"}'
+	
+	sleep 1m
+
+done
+
+# to run the script call ./run_monitoring.sh >> file_name
