932545a905bc6dda81a139949acee813ebf980fb
ezambomsantana <ezambomsantana@gmail.com>
2018-01-05 23:51:29 -0200

    some code refactoring

--- src/traffic_models.erl
+++ src/traffic_models.erl
@@ -5,51 +5,31 @@
    get_speed_walk/1
   ]).
 
-get_speed_car( Data ) ->
+get_speed_car( LinkData ) ->
 
-	Id = element( 2 , Data ), % Link Id
-	Length = element( 3 , Data ), % Link Length	
-	Capacity = element( 4 , Data )  / 2,
-	Freespeed = element( 5 , Data ), 	
-	NumberCars = element( 6 , Data ), 
-
-	% Calculate car speed
-	Density = ( NumberCars ),
-
-	MaximumDensity = Capacity,
+	{ _ , Id , Length , Capacity , Freespeed , NumberCars } = LinkData,
 
 	MinimumDensity = (Capacity * 0.3) 	 ,
-
-	Speed = case Density > MinimumDensity of
+	Speed = case NumberCars > MinimumDensity of
 
 		true ->
-
-			case Density >= MaximumDensity of
-
+			case NumberCars >= Capacity of
 				true ->
 					0.8;
 				false ->
-			
-					Number = math:pow ( 1 - ( Density / MaximumDensity ) , 0.6),
-					Freespeed * (Number)
-	
+					Freespeed * math:pow ( 1 - ( NumberCars / Capacity ) , 0.6)
 			end;
-			
 		false ->
-		
-			Freespeed 
-
+			Freespeed
 	end,
 
 	Time = ( Length / Speed ) + 1,
-
 	{ Id , round( Time ) , round ( Length ) }.
 
 
-get_speed_walk( Data ) ->
+get_speed_walk( LinkData ) ->
 
-	Id = element( 2 , Data ), % Link Id
-	Length = element( 3 , Data ), % Link Length	
+	{ _ , Id , Length , _ , _ , _ } = LinkData,	
 
 	Time = ( Length / 2 ) + 1,
 
